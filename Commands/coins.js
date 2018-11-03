48108c401905864388bb198ab8b1e209570a261c
ButchBullly <monspek@protonmail.com>
2018-11-03 00:09:27 -0400

    updates coin system in to work with the new database system

--- Commands/coins.js
+++ Commands/coins.js
@@ -1,26 +1,45 @@
-const Discord = require("discord.js");
+const Discord = require("discord.js"),
+	mongoose = require('mongoose');
 let coins = require("../coins.json");
-const botconfig = require("../botconfig.json");
+const botconfig = require("../botconfig.json"),
+	Money = require("../models/money.js");
+
+mongoose.connect("mongodb://localhost:27017/DoggoChan", {
+	useNewUrlParser: true
+});
 
 module.exports.run = async (bot, message, args) => {
-  if(!coins[message.author.id]){
-    coins[message.author.id] = {
-      coins: 0
-    };
-  }
+	// if (!coins[message.author.id]) {
+	// 	coins[message.author.id] = {
+	// 		coins: 0
+	// 	};
+	// }
 
-  let uCoins = coins[message.author.id].coins;
+	Money.findOne({
+		userID: message.author.id,
+		serverID: message.guild.id
+	}, (err, money) => {
+		if(err) console.log(err);
 
-  let coinEmbed = new Discord.RichEmbed()
-  .setAuthor(message.author.username)
-  .setColor(botconfig.doggo)
-  .addField("💸", uCoins);
+		let embed = new Discord.RichEmbed()
+			.setAuthor(message.author.username)
+			.setColor(botconfig.doggo)
+			.setThumbnail(message.author.displayAvatarURL)
 
-  message.delete().catch(O_o=>{});
-  message.channel.send(coinEmbed).then(msg => {msg.delete(5000)});
+		if(!money) {
+			embed.addField("Coins", "0", true);
+			return message.channel.send(embed).then(msg => { msg.delete(5000) });
+		} else if (money) {
+			embed.addField("Coins", money.money, true);
+			return message.channel.send(embed).then(msg => { msg.delete(5000) });
+		} else {
+			message.channel.send("Error")
+		}
+	})
 
+	message.delete().catch(O_o => { });
 }
 
 module.exports.help = {
-  name: "coins"
+	name: "coins"
 }
