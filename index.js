71d623971135405c77f043658f4ad3e081a30762
ButchBullly <monspek@protonmail.com>
2018-11-03 00:00:36 -0400

    major changes
    changed index.js arround to make code cleaner and more efficent also worked on a mongoosedb coin system

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ bot.commands = new Discord.Collection();
 const botconfig = require("./botconfig.json"),
 	activities = require("./assets/activity.json"),
 	bList = require("./assets/blacklist.json"),
-	errors = require("./utils/errors.js");
+	errors = require("./utils/errors.js"),
+	Money = require("./models/money.js");
 let coins = require("./coins.json");
 let xp = require("./xp.json");
 
-//mongoose.connect('mongodb://localhost/Reports');
+mongoose.connect('mongodb://localhost:27017/DoggoChan', {
+	useNewUrlParser: true
+});
 
 fs.readdir("./commands/", (err, files) => {
 
@@ -44,35 +47,6 @@ bot.on("message", async message => {
 		};
 	}
 
-	if (message.author.bot) return;
-	if (message.channel.type === "dm") return;
-
-	let xpAdd = Math.floor(Math.random() * 7) + 8;
-
-	if (!xp[message.author.id]) {
-		xp[message.author.id] = {
-			xp: 0,
-			level: 1
-		};
-	}
-
-	let curxp = xp[message.author.id].xp;
-	let curlvl = xp[message.author.id].level;
-	let nxtLvl = xp[message.author.id].level * 300;
-	xp[message.author.id].xp = curxp + xpAdd;
-	if (nxtLvl <= xp[message.author.id].xp) {
-		xp[message.author.id].level = curlvl + 1;
-		let lvlup = new Discord.RichEmbed()
-			.setTitle("Level Up!")
-			.setColor(botconfig.doggo)
-			.addField("New Level", curlvl + 1);
-
-		message.channel.send(lvlup).then(msg => { msg.delete(5000) });
-	}
-	fs.writeFile("./xp.json", JSON.stringify(xp), (err) => {
-		if (err) console.log(err)
-	});
-
 	//for my server only
 	if (message.guild.id === "498112893330391041") {
 		let wordfound = false;
@@ -86,39 +60,99 @@ bot.on("message", async message => {
 		}
 	}
 
-	let prefix = prefixes[message.guild.id].prefixes;
-	if (!message.content.startsWith(prefix)) return;
-
-	if (!coins[message.author.id]) {
-		coins[message.author.id] = {
-			coins: 0
-		};
-	}
-
-	let coinAmt = Math.floor(Math.random() * 15) + 1;
-	let baseAmt = Math.floor(Math.random() * 15) + 1;
-
-	if (coinAmt == baseAmt) {
-		coins[message.author.id] = {
-			coins: coins[message.author.id].coins + coinAmt
-		};
-		fs.writeFile("./coins.json", JSON.stringify(coins), (err) => {
-			if (err) console.log(err);
-		});
-		let coinEmbed = new Discord.RichEmbed()
-			.setAuthor(message.author.username)
-			.setColor(botconfig.doggo)
-			.addField("💸", `${coinAmt} coins given to ${message.author}! 🔥`);
-
-		message.channel.send(coinEmbed).then(msg => { msg.delete(5000) });
-	}
-
 	let messageArray = message.content.split(" ");
 	let cmd = messageArray[0];
 	let args = messageArray.slice(1);
 
-	let commandfile = bot.commands.get(cmd.slice(prefix.length));
-	if (commandfile) commandfile.run(bot, message, args);
+	let prefix = prefixes[message.guild.id].prefixes;
+	if(message.content.startsWith(prefix) && !message.author.bot && message.channel.type !== "dm") {
+		let commandfile = bot.commands.get(cmd.slice(prefix.length));
+		if (commandfile) commandfile.run(bot, message, args);
+	} else if(!message.author.bot && message.channel.type !== "dm") {
+		// if (!coins[message.author.id]) {
+		// 	coins[message.author.id] = {
+		// 		coins: 0
+		// 	};
+		// }
+	
+		// let coinAmt = Math.floor(Math.random() * 15) + 1;
+		// let baseAmt = Math.floor(Math.random() * 15) + 1;
+	
+		// if (coinAmt == baseAmt) {
+		// 	coins[message.author.id] = {
+		// 		coins: coins[message.author.id].coins + coinAmt
+		// 	};
+		// 	fs.writeFile("./coins.json", JSON.stringify(coins), (err) => {
+		// 		if (err) console.log(err);
+		// 	});
+		// 	let coinEmbed = new Discord.RichEmbed()
+		// 		.setAuthor(message.author.username)
+		// 		.setColor(botconfig.doggo)
+		// 		.addField("💸", `${coinAmt} coins given to ${message.author}! 🔥`);
+	
+		// 	message.channel.send(coinEmbed).then(msg => { msg.delete(5000) });
+		// }
+	
+		let coinstoadd = Math.floor(Math.random() * 50) + 1;
+		let coinsneeded = Math.floor(Math.random() * 50) + 1;
+		if (coinstoadd == coinsneeded){
+			let moneyEmbed = new Discord.RichEmbed()
+				.setAuthor(message.author.username)
+				.setColor(botconfig.doggo)
+				.addField("💸", `${coinstoadd} coins given to ${message.author}! 🔥`);
+
+			Money.findOne({
+				userID: message.author.id,
+				serverID: message.guild.id
+			}, (err, money) => {
+				if (err) console.log(err);
+
+				if (!money) {
+					const newMoney = new Money({
+						userID: message.author.id,
+						serverID: message.guild.id,
+						money: coinstoadd
+					})
+
+					newMoney.save().catch(err => console.log(err));
+
+					message.channel.send(moneyEmbed).then(msg => {msg.delete(5000)});
+				} else {
+					money.money = money.money + coinstoadd;
+
+					money.save().catch(err => console.log(err));
+
+					message.channel.send(moneyEmbed).then(msg => {msg.delete(5000)});
+				}
+			})
+		}
+		
+		let xpAdd = Math.floor(Math.random() * 7) + 8;
+
+		if (!xp[message.author.id]) {
+			xp[message.author.id] = {
+				xp: 0,
+				level: 1
+			};
+		}
+
+		let curxp = xp[message.author.id].xp;
+		let curlvl = xp[message.author.id].level;
+		let nxtLvl = xp[message.author.id].level * 300;
+		xp[message.author.id].xp = curxp + xpAdd;
+		if (nxtLvl <= xp[message.author.id].xp) {
+			xp[message.author.id].level = curlvl + 1;
+			let lvlup = new Discord.RichEmbed()
+				.setTitle("Level Up!")
+				.setColor(botconfig.doggo)
+				.addField("New Level", curlvl + 1);
+
+			message.channel.send(lvlup).then(msg => { msg.delete(5000) });
+		}
+		fs.writeFile("./xp.json", JSON.stringify(xp), (err) => {
+			if (err) console.log(err)
+		});
+	}
 
 });
 
