e6dca83a99aa28404eb215d0abff61090cc85a8b
ButchBullly <monspek@protonmail.com>
2018-11-03 11:43:55 -0400

    minor changes

--- Commands/pay.js
+++ Commands/pay.js
@@ -1,55 +1,90 @@
-const Discord = require("discord.js");
-const fs = require("fs");
+const Discord = require("discord.js"),
+	mongoose = require("mongoose");
+const config = require("../botconfig.json"),
+	Money = require("../models/money.js");
+
+mongoose.connect("mongodb://localhost:27017/DoggoChan", {
+	useNewUrlParser: true
+});
 
 // TODO: port the json system to the mongooseDB
 
 module.exports.run = async (bot, message, args) => {
-  //!pay @isatisfied 59345
+	//!pay @isatisfied 59345
 
-  if(!coins[message.author.id]){
-    return message.reply("You don't have any coins!")
-  }
+	Money.findOne({
+		userID: message.author.id,
+		serverID: message.guild.id
+	}, (err, money) => {
+		if(err) console.log(err);
 
-  let pUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+		let embed = new Discord.RichEmbed()
+			.setAuthor(message.author.username)
+			.setColor(config.doggo)
+			.setThumbnail(message.author.displayAvatarURL)
 
-  if(!coins[pUser.id]){
-    coins[pUser.id] = {
-      coins: 0
-    };
-  }
+		if(!money) {
+			embed.addField("ERROR:", "You do not have enough coins!", true);
+			return message.channel.send(embed).then(msg => { msg.delete(5000) });
+		} else if (money) {
+			Money.find({
+				serverID: message.guild.id,
+				userID: message.author,
+				money: money.money
+			})
 
-  let pCoins = coins[pUser.id].coins;
-  let sCoins = coins[message.author.id].coins;
+			embed.addField("Sent!", `${message.author} has given ${pUser} ${args[0]} coins!`, true);
+			return message.channel.send(embed).then(msg => { msg.delete(5000) });
+		} else {
+			message.channel.send("Error")
+		}
+	})
 
-  if(sCoins < args[1]) return message.reply("Not enough coins there!");
+	//! Old Code
+	// if (!coins[message.author.id]) {
+	// 	return message.reply("You don't have any coins!")
+	// }
 
-  if(isNaN(args[1])) {
-    return message.reply("This is not a number! GRR!!");
-  }
-  if(args[1] <= 0) {
-    return message.reply("That number will not work!")
-  }
-  if(pUser.id == message.author.id) {
-    return message.reply("You can't send to your self!")
-  }
+	// let pUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
 
-  coins[message.author.id] = {
-    coins: sCoins - parseInt(args[1])
-  };
+	// if (!coins[pUser.id]) {
+	// 	coins[pUser.id] = {
+	// 		coins: 0
+	// 	};
+	// }
 
-  coins[pUser.id] = {
-    coins: pCoins + parseInt(args[1])
-  };
+	// let pCoins = coins[pUser.id].coins;
+	// let sCoins = coins[message.author.id].coins;
 
-  message.channel.send(`${message.author} has given ${pUser} ${args[1]} coins.`);
+	// if (sCoins < args[1]) return message.reply("Not enough coins there!");
 
-  fs.writeFile("./coins.json", JSON.stringify(coins), (err) => {
-    if(err) cosole.log(err)
-  });
+	// if (isNaN(args[1])) {
+	// 	return message.reply("This is not a number! GRR!!");
+	// }
+	// if (args[1] <= 0) {
+	// 	return message.reply("That number will not work!")
+	// }
+	// if (pUser.id == message.author.id) {
+	// 	return message.reply("You can't send to your self!")
+	// }
+
+	// coins[message.author.id] = {
+	// 	coins: sCoins - parseInt(args[1])
+	// };
+
+	// coins[pUser.id] = {
+	// 	coins: pCoins + parseInt(args[1])
+	// };
+
+	// message.channel.send(`${message.author} has given ${pUser} ${args[1]} coins.`);
+
+	// fs.writeFile("./coins.json", JSON.stringify(coins), (err) => {
+	// 	if (err) cosole.log(err)
+	// });
 
 
 }
 
 module.exports.help = {
-  name: "pay"
+	name: "pay"
 }
