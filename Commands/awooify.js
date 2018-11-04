2eede9ec24137c61ec82accf232494948b82cf82
ButchBullly <monspek@protonmail.com>
2018-11-03 20:31:20 -0400

    added .awooify

---
+++ Commands/awooify.js
@@ -0,0 +1,30 @@
+const fetch = require('node-superfetch');
+
+module.exports.run = async (bot, message, args) => {
+    let user = message.mentions.users.first();
+    if (!user) {
+        fetch.get(`https://nekobot.xyz/api/imagegen?type=awooify&url=${message.author.avatarURL}`).then(res =>
+            message.channel.send({
+                "embed": {
+                    "color": 545762,
+                    "image": {
+                        "url": res.body.message
+                    }
+                }
+            }));
+    } else {
+        fetch.get(`https://nekobot.xyz/api/imagegen?type=awooify&url=${user.avatarURL}`).then(res =>
+            message.channel.send({
+                "embed": {
+                    "color": 545762,
+                    "image": {
+                        "url": res.body.message
+                    }
+                }
+            }));
+    }
+}
+
+module.exports.help = {
+    name: "awooify"
+}
