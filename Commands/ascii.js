a31e025ee19c9a034cfbcfb959a743496bf971e2
ButchBullly <monspek@protonmail.com>
2018-11-04 12:44:16 -0500

    removed old code

--- Commands/ascii.js
+++ Commands/ascii.js
@@ -3,18 +3,9 @@ const Discord = require("discord.js");
 const bList = require("../assets/blacklist.json");
 
 module.exports.run = async (bot, message, args, ops) => {
-    if(message.guild.id === "498112893330391041"){
-        let wordfound = false;
-        for(var i in bList.words) {
-          if(message.content.toLowerCase().includes(bList.words[i].toLowerCase())) wordfound = true;
-        }
-    
-        if(wordfound) return;
-    }
-
-    ascii.font(args.join(" "), "Doom", function(rendered) {
+    ascii.font(args.join(" "), "Doom", function (rendered) {
         rendered = rendered.trimRight();
-        if(rendered.length > 2000) return message.channel.send("Sorry, this message is too long.");
+        if (rendered.length > 2000) return message.channel.send("Sorry, this message is too long.");
 
         message.channel.send(rendered, {
             code: "md"
