6b8f9a89be34adc8236d89e7cc5fb9c9e49a5b07
ButchBullly <monspek@protonmail.com>
2018-11-04 13:23:12 -0500

    added trump command

---
+++ Commands/trump.js
@@ -0,0 +1,22 @@
+const fetch = require('node-superfetch');
+
+module.exports.run = async (bot, message, args) => {
+    let text = args.join(" ");
+    if (!text) {
+        message.channel.send("You didn't give me any text!").then(msg => {msg.delete(5000)});
+    } else {
+        fetch.get(`https://nekobot.xyz/api/imagegen?type=TrumpTweet&text=${text}`).then(res =>
+            message.channel.send({
+                "embed": {
+                    "color": 545762,
+                    "image": {
+                        "url": res.body.message
+                    }
+                }
+            }));
+    }
+}
+
+module.exports.help = {
+    name: "trump"
+}
