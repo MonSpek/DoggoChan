df45bf1a3b16196dad4a9671c30941e65a5f7c66
ButchBullly <monspek@protonmail.com>
2018-11-05 19:45:29 -0500

    added fry command

---
+++ Commands/fry.js
@@ -0,0 +1,38 @@
+const fetch = require('node-superfetch');
+
+module.exports.run = async (bot, message, args) => {
+    let image = args[0];
+    if (!image) {
+        fetch.get(`https://nekobot.xyz/api/imagegen?type=Deepfry&image=${message.author.avatarURL}`).then(res =>
+            message.channel.send({
+                "embed": {
+                    "color": 545762,
+                    "image": {
+                        "url": res.body.message
+                    },
+                    "footer": {
+                        "text": `Done by ${message.author.username}`
+                    }
+                }
+            }));
+    } else {
+        fetch.get(`https://nekobot.xyz/api/imagegen?type=Deepfry&image=${image}`).then(res =>
+        message.channel.send({
+            "embed": {
+                "color": 545762,
+                "image": {
+                    "url": res.body.message
+                },
+                "footer": {
+                    "text": `Done by ${message.author.username}`
+                }
+            }
+        }));
+    }
+}
+
+module.exports.help = {
+    name: "fry",
+    role: "fun",
+    description: "Puts a picture on an iPhone screen"
+}
