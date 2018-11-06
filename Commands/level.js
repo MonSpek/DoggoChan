c96e12165b908ac0a589399bc76e39e9ad74e26a
ButchBullly <monspek@protonmail.com>
2018-11-06 09:13:58 -0500

    ported level system to mongoose

--- Commands/level.js
+++ Commands/level.js
@@ -1,30 +1,69 @@
-const Discord = require("discord.js");
-const botconfig = require("../botconfig");
-let xp = require("../xp.json");
+const Discord = require("discord.js"),
+	mongoose = require("mongoose");
+const botconfig = require("../botconfig"),
+	xpMongoose = require("../models/xp.js");
+//let xp = require("../xp.json");
+
+mongoose.connect('mongodb://localhost:27017/DoggoChan', {
+	useNewUrlParser: true
+});
 
 module.exports.run = async (bot, message, args) => {
+	await message.delete();
 
-	if (!xp[message.author.id]) {
-		xp[message.author.id] = {
-			xp: 0,
-			level: 1
-		};
-	}
+	// if (!xp[message.author.id]) {
+	// 	xp[message.author.id] = {
+	// 		xp: 0,
+	// 		level: 1
+	// 	};
+	// }
 
-	let curxp = xp[message.author.id].xp;
-	let curlvl = xp[message.author.id].level;
-	let nxtLvlXp = curlvl * 300;
-	let difference = nxtLvlXp - curxp;
+	// let curxp = xp[message.author.id].xp;
+	// let curlvl = xp[message.author.id].level;
+	// let nxtLvlXp = curlvl * 300;
+	// let difference = nxtLvlXp - curxp;
 
-	let lvlEmbed = new Discord.RichEmbed()
-	.setAuthor(message.author.username)
-	.setColor(botconfig.doggo)
-	.addField("Level", curlvl, true)
-	.addField("XP", curxp, true)
-	.setFooter(`${difference} XP til level up`, message.author.displayAvatarURL);
+	// let lvlEmbed = new Discord.RichEmbed()
+	// 	.setAuthor(message.author.username)
+	// 	.setColor(botconfig.doggo)
+	// 	.addField("Level", curlvl, true)
+	// 	.addField("XP", curxp, true)
+	// 	.setFooter(`${difference} XP til level up`, message.author.displayAvatarURL);
 
-	message.delete();
-	message.channel.send(lvlEmbed).then(msg => { msg.delete(10000) });
+	// message.delete();
+	// message.channel.send(lvlEmbed).then(msg => { msg.delete(10000) });
+
+	xpMongoose.findOne({
+		userID: message.author.id,
+		serverID: message.guild.id
+	}, (err, xp) => {
+		if(err) console.log(err);
+
+		if (!xp) {
+			const newXP = new xpMongoose({
+				userID: message.author.id,
+				serverID: message.guild.id,
+				xp: 0,
+				level: 1
+			})
+
+			newXP.save().catch(err => console.log(err));
+		} else {
+			let curxp = xp.xp;
+			let curlvl = xp.level;
+			let nxtLvlxp = curlvl * 300;
+			let difference = nxtLvlxp - curxp;
+
+			let lvlEmbed = new Discord.RichEmbed()
+				.setAuthor(`${message.author.username}`, message.author.displayAvatarURL)
+				.setColor(botconfig.doggo)
+				.addField("Level", curlvl, true)
+				.addField("XP", curxp, true)
+				.setFooter(`${difference} XP til level up`);
+
+			message.channel.send(lvlEmbed).then(msg => { msg.delete(10000) });
+		}
+	})
 }
 
 module.exports.help = {
--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const botconfig = require("./botconfig.json"),
 	errors = require("./utils/errors.js"),
 	xpMongoose = require("./models/xp.js"),
 	Money = require("./models/money.js");
-let xp = require("./xp.json");
 
 mongoose.connect('mongodb://localhost:27017/DoggoChan', {
 	useNewUrlParser: true
@@ -195,8 +194,6 @@ bot.on("message", async message => {
 			})
 		}
 
-
-		//!Old code
 		let xpAdd = Math.floor(Math.random() * 7) + 8;
 
 		xpMongoose.findOne({
@@ -235,32 +232,6 @@ bot.on("message", async message => {
 				xp.save().catch(err => console.log(err));
 			}
 		})
-
-		// if (!xp[message.author.id]) {
-		// 	xp[message.author.id] = {
-		// 		xp: 0,
-		// 		level: 1
-		// 	};
-		// }
-
-		// let curxp = xp[message.author.id].xp;
-		// let curlvl = xp[message.author.id].level;
-		// let nxtLvl = xp[message.author.id].level * 300;
-		// xp[message.author.id].xp = curxp + xpAdd;
-		// if (nxtLvl <= xp[message.author.id].xp) {
-		// 	xp[message.author.id].level = curlvl + 1;
-			// let lvlup = new Discord.RichEmbed()
-			// 	.setTitle("Level Up!")
-			// 	.setColor(botconfig.doggo)
-			// 	.addField("New Level", curlvl + 1);
-
-		// 	message.channel.send(lvlup).then(msg => { msg.delete(5000) });
-		// }
-		// fs.writeFile("./xp.json", JSON.stringify(xp), (err) => {
-		// 	if (err) console.log(err)
-		// });
-
-
 	}
 
 });
