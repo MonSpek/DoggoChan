446bbc3cce5ddab742a89ef8aed63f1b02d1954f
ButchBullly <monspek@protonmail.com>
2018-11-06 17:41:42 -0500

    Update Commands/advice.js

--- Commands/advice.js
+++ Commands/advice.js
@@ -6,7 +6,6 @@ module.exports.run = async (bot, message, args) => {
     let cn = request("http://api.adviceslip.com/advice", function (err, res, body) {
         try {
             let cnj = JSON.parse(body)
-            // message.channel.send(cnj.slip.advice).then(msg => { msg.delete(20000) })
             let embed = new Discord.RichEmbed()
                 .setAuthor(`${message.author.username}`, message.author.displayAvatarURL)
                 .setDescription(`**${cnj.slip.advice}**`)
