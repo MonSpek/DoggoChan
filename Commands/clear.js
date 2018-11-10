dd5a9549fb1e273d57f6220088db4de7e10b7255
ButchBullly <monspek@protonmail.com>
2018-11-10 13:28:17 -0500

    improved clear command

--- Commands/clear.js
+++ Commands/clear.js
@@ -2,6 +2,8 @@ const Discord = require("discord.js");
 const errors = require("../utils/errors.js");
 
 module.exports.run = async (bot, message, args) => {
+	await message.delete();
+
 	if (!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
 	if (!args[0]) return message.channel.send("You got to tell me how many messages you want me to delete");
 	message.channel.bulkDelete(args[0]).then(() => {
