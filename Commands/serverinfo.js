7473ca5f30a6fa15f3ba0dce4e26a34035a2d3d8
ButchBullly <monspek@protonmail.com>
2018-11-16 18:44:26 -0500

    updated server info

--- Commands/serverinfo.js
+++ Commands/serverinfo.js
@@ -2,15 +2,19 @@ const Discord = require("discord.js");
 const botconfig = require("../botconfig.json");
 
 module.exports.run = async (bot, message, args) => {
-	let sicon = message.guild.iconURL;
 	let serverembed = new Discord.RichEmbed()
-	.setDescription("Server Information")
-	.setColor(botconfig.doggo)
-	.setThumbnail(sicon)
-	.addField("Server Name", message.guild.name)
-	.addField("Created On", message.guild.createdAt)
-	.addField("You Joined", message.member.joinedAt)
-	.addField("Total Members", message.guild.memberCount);
+		.setAuthor(message.guild.name, message.guild.iconURL)
+		.setDescription("Server Information")
+		.setColor(botconfig.doggo)
+		.setThumbnail(message.guild.iconURL)
+		.addField("Server Name", message.guild.name, true)
+		.addField("Server ID", message.guild.id, true)
+		.addField("Server Owner", `${message.guild.owner.user.username}#${message.guild.owner.user.discriminator}`, true)
+		.addField("Channels", message.guild.channels.size, true)
+        .addField("Roles", message.guild.roles.size, true)
+		.addField("Created On", `${message.channel.guild.createdAt.toUTCString().substr(0, 16)} (${checkDays(message.channel.guild.createdAt)})`, true)
+		.addField("You Joined", message.member.joinedAt, true)
+		.addField("Total Members", message.guild.memberCount, true);
 
 	message.channel.send(serverembed);
 }
